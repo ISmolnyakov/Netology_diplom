6dba20eda754311c0c89339070932d1aee6d82f4
ISmolnyakov <91562953+ISmolnyakov@users.noreply.github.com>
2023-03-03 18:50:42 +0700

    config update

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
+group_token = ''
+user_token = ''
+
+offset = 0 #сдвиг
+line = range(0, 1000) # последовательность для перебора найденных пользователей
 
 
-
+db_name = 'vk_users_db'
+user = 'postgres'
+password = ''
